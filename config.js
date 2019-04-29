f00621166bea20a8bc78f1a41389d8bcc77d20a3
Carsten <18394207+Aschratt@users.noreply.github.com>
2019-04-29 15:56:17 +0200

    Config import

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'Carsten\'s Blog', // Required
+  author: 'Carsten Rudolph', // Required
+  description: 'Software Developer & Video Games Enthusiast',
+  primaryColor: '#3F9FCF', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://www.aschratt.com',
+    github: 'https://github.com/aschratt',
+    twitter: 'https://twitter.com/@aschratt',
+    //linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://aschratt.github.io/devblog/',
 };
